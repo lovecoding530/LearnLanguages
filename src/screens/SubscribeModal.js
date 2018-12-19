7c2cf690f3e1f07986e815db85d894632e3a5c2b
Unknown <lovecoding530@gmail.com>
2018-12-19 15:44:06 -0800

    terms of service and privacy policy, change icon

--- src/screens/SubscribeModal.js
+++ src/screens/SubscribeModal.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Button, FlatList, Image, TouchableOpacity, Modal, Picker, TouchableWithoutFeedback} from 'react-native';
+import {StyleSheet, Text, View, Button, Modal, Linking} from 'react-native';
 import { strings } from '../i18n';
+
+const PRIVACY_POLICY_URL = "https://www.freeprivacypolicy.com/privacy/view/49009ffebb8f813a5fc29a1bb04f1bc0"
+const TERMS_OF_SERVICE_URL = "https://sbslanguage.com/tos"
 
 export default class SubscribeModal extends Component {
 
@@ -19,8 +22,22 @@ export default class SubscribeModal extends Component {
                         <Text style={styles.desc}>
                             To use this app, A $1.49/month purchase will be applied to your iTunes account at the end of the trial.
                             Subscriptions will automatically renew unless canceled within 24-hours before the end of the current period. You can cancel anytime with your iTunes account settings. Any unused portion of a free trial will be forfeited if you purchase a subscription.                        </Text>
-                        <Button title={"Terms of Service"} onPress={()=>{}}/>
-                        <Button title={"Privacy Policy"} onPress={()=>{}}/>
+                        <Button 
+                            title={"Terms of Service"} 
+                            onPress={()=>{
+                                Linking.openURL(TERMS_OF_SERVICE_URL).catch((err) =>
+                                    console.error('An error occurred', err) 
+                                )
+                            }}
+                        />
+                        <Button 
+                            title={"Privacy Policy"} 
+                            onPress={()=>{
+                                Linking.openURL(PRIVACY_POLICY_URL).catch((err) =>
+                                    console.error('An error occurred', err) 
+                                )
+                            }}
+                        />
                         <View style={styles.buttonBar}>
                             <Button title={strings("Cancel")} onPress={onCancel}/>
                             <Button title={strings("OK")} onPress={onOK}/>
